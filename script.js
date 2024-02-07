0530220c090681c4215867ff948ac1db74394d81
Hayats <hayatudeenabdulrahman@gmail.com>
2024-02-08 00:19:07 +0100

    updated resume

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+  // Toggle the 'dark-mode' class on the body
+  document.body.classList.toggle('dark-mode');
+  console.log('toggled!!!!');
+  
+  // Check if dark mode is enabled and save the user preference
+  const isDarkModeEnabled = document.body.classList.contains('dark-mode');
+  localStorage.setItem('darkModeEnabled', isDarkModeEnabled);
+}
+
+// Check for user's dark mode preference on page load
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModePreference = localStorage.getItem('darkModeEnabled');
+  
+  // If the user previously enabled dark mode, apply it on page load
+  if (darkModePreference === 'true') {
+    document.body.classList.add('dark-mode');
+  }
+});
